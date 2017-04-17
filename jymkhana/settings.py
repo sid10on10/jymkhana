0c4562e88732b6dac7d7a7af51376a299c31fbce
siddhant kumar singh <sid10on10@gmail.com>
2017-04-17 11:11:40 +0530

    commit message

--- jymkhana/settings.py
+++ jymkhana/settings.py
@@ -14,6 +14,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -102,17 +103,13 @@ USE_L10N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.8/howto/static-files/
-#STATIC_ROOT = ''
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ( os.path.join('static'), )
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static/')
-#for compressor app
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    # other finders..
-    'compressor.finders.CompressorFinder',
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
 )
+
 
