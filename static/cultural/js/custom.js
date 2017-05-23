0b27af2845835f37639518cbbaacb0c6ca13bd87
siddhant kumar singh <sid10on10@gmail.com>
2017-05-24 00:58:51 +0530

    commit message

--- static/cultural/js/custom.js
+++ static/cultural/js/custom.js
@@ -47,27 +47,4 @@
 	
 	
 	
-//Header Background Image Transition
-	$("header").bgswitcher({
-  images: [{% static "cultural/img/header1.jpg" %}, {% static "cultural/img/header2.jpg" %}, {% static "cultural/img/header3.jpg" %}, {% static "cultural/img/header4.jpg" %}],
-  effect: "blind",
-  interval: 4000,
-  duration: 1000
-});
 
-/*Caption change JavaScript for Header background*/	
-
- $("#caption-text").Morphext({
-    // The [in] animation type. Refer to Animate.css for a list of available animations.
-    animation: "zoomIn",
-    // An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
-    separator: "_",
-    // The delay between the changing of each phrase in milliseconds.
-    speed: 4000,
-    complete: function () {
-        // Called after the entrance animation is executed.
-    }
-});
-
- 
- })(jQuery); // End of use strict
