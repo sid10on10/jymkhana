8a455d0ce3227f167a4dede64d7a6c15c01ae86b
siddhant kumar singh <sid10on10@gmail.com>
2017-05-24 01:50:08 +0530

    commit message

--- static/cultural/js/custom.js
+++ static/cultural/js/custom.js
@@ -3,7 +3,7 @@
  * Code licensed under the Apache License v2.0.
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
-{% load staticfiles %}
+
 (function($) {
     "use strict"; // Start of use strict
 
@@ -45,6 +45,31 @@
  // Initialize WOW.js Scrolling Animations
     new WOW().init();
 	
+
+//Header Background Image Transition
+	$("header").bgswitcher({
+  images: ["/static/cultural/img/header1.jpg", "/static/cultural/img/header2.jpg", "/static/cultural/img/header3.jpg", "/static/cultural/img/header4.jpg"],
+  effect: "blind",
+  interval: 4000,
+  duration: 1000
+});
+
+/*Caption change JavaScript for Header background*/	
+
+ $("#caption-text").Morphext({
+    // The [in] animation type. Refer to Animate.css for a list of available animations.
+    animation: "zoomIn",
+    // An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
+    separator: "_",
+    // The delay between the changing of each phrase in milliseconds.
+    speed: 4000,
+    complete: function () {
+        // Called after the entrance animation is executed.
+    }
+});
+
+ 
+ })(jQuery); // End of use strict
 	
 	
 
