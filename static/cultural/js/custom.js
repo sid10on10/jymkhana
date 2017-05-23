d5a8f103b6fbccac7bc0508fab944da3e3c0e6ea
siddhant kumar singh <sid10on10@gmail.com>
2017-05-24 04:04:39 +0530

    commit message

--- static/cultural/js/custom.js
+++ static/cultural/js/custom.js
@@ -42,29 +42,3 @@
     new WOW().init();
 	
 
-//Header Background Image Transition
-	$("header").bgswitcher({
-  images: ["/static/fmc/clubs/animation/img/header1.jpg", "/static/fmc/clubs/animation/img/header2.jpg", "/static/fmc/clubs/animation/img/header3.jpg", "/static/fmc/clubs/animation/img/header4.jpg"],
-  effect: "fade",
-  interval: 4000,
-  duration: 1000
-});
-
-/*Caption change JavaScript for Header background*/	
-
- $("#caption-text").Morphext({
-    // The [in] animation type. Refer to Animate.css for a list of available animations.
-    animation: "fadeInUp",
-    // An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
-    separator: "_",
-    // The delay between the changing of each phrase in milliseconds.
-    speed: 4000,
-    complete: function () {
-        // Called after the entrance animation is executed.
-    }
-});
-
-
- 
- 
- })(jQuery); // End of use strict
