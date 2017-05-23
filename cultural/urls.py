59c48881f3c50c09a7d7bb6873db84b6ed263807
siddhant kumar singh <sid10on10@gmail.com>
2017-05-23 16:18:34 +0530

    commit message

--- cultural/urls.py
+++ cultural/urls.py
@@ -6,9 +6,9 @@ from . import views
 urlpatterns = [
 	
 	url(r'^$', views.cultural, name='cultural'),
-    url(r'^Clubs/fac$', views.animation, name='fac'),
-	url(r'^Clubs/imc$', views.cine, name='imc'),
-	url(r'^Clubs/lit$', views.design, name='lit'),
-	url(r'^Clubs/masq$', views.media, name='masq'),
-	url(r'^Clubs/quiz$', views.outreach, name='quiz'),
+    url(r'^Clubs/fac$', views.fac, name='fac'),
+	url(r'^Clubs/imc$', views.imc, name='imc'),
+	url(r'^Clubs/lit$', views.lit, name='lit'),
+	url(r'^Clubs/masq$', views.masq, name='masq'),
+	url(r'^Clubs/quiz$', views.quiz, name='quiz'),
 ]
