8baecad93a13c1cc3df18266f6e062609e26c3a3
Kjain16 <kartavya.jain16@gmail.com>
2017-07-03 10:57:08 +0530

    Update urls.py

--- sntc/urls.py
+++ sntc/urls.py
@@ -6,11 +6,12 @@ from . import views
 urlpatterns = [
 	
 	url(r'^$', views.sntc, name='sntc'),
-    url(r'^clubs/aero_modelling$', views.aero_modelling, name='aero_modelling'),
+    	url(r'^clubs/aero_modelling$', views.aero_modelling, name='aero_modelling'),
 	url(r'^clubs/astronomy$', views.astronomy, name='astronomy'),
+	url(r'^clubs/clubofeconomicsandfinance$', views.clubofeconomicsandfinance, name='clubofeconomicsandfinance'),
 	url(r'^clubs/robotics$', views.robotics, name='robotics'),
 	url(r'^clubs/sae_collegiate$', views.sae_collegiate, name='sae_collegiate'),
 	url(r'^clubs/cops$', views.cops, name='cops'),
 	url(r'^clubs/green$', views.green, name='green'),
-	url(r'^clubs/clubofeconomicsandfinance$', views.clubofeconomicsandfinance, name='clubofeconomicsandfinance'),
+	url(r'^clubs/troc$', views.troc, name='troc'),
 ]
