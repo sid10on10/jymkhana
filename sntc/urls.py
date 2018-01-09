f3562c50fd4a42ff7336d86a3b0d07a84e52cd27
Siddhant Singh <sid10on10@gmail.com>
2018-01-09 13:07:56 +0530

    sntc integration

--- sntc/urls.py
+++ sntc/urls.py
@@ -4,13 +4,17 @@ from . import views
 
 
 urlpatterns = [
-	
-	url(r'^$', views.sntc, name='sntc'),
-    url(r'^clubs/aero_modelling$', views.aero_modelling, name='aero_modelling'),
-	url(r'^clubs/astronomy$', views.astronomy, name='astronomy'),
-	url(r'^clubs/robotics$', views.robotics, name='robotics'),
-	url(r'^clubs/sae_collegiate$', views.sae_collegiate, name='sae_collegiate'),
-	url(r'^clubs/cops$', views.cops, name='cops'),
-	url(r'^clubs/green$', views.green, name='green'),
-	url(r'^clubs/clubofeconomicsandfinance$', views.clubofeconomicsandfinance, name='clubofeconomicsandfinance'),
+	url(r'^$', 'sntc.views.home', name='home'),
+    url(r'^clubs/aero_modelling$', 'sntc.views.amc', name='amc'),
+    url(r'^clubs/astronomy$', 'sntc.views.astro', name='astro'),
+    url(r'^clubs/cops$', 'sntc.views.cops', name='cops'),
+    url(r'^clubs/green$', 'sntc.views.green', name='green'),
+    url(r'^clubs/troc$', 'sntc.views.troc', name='troc'),
+    url(r'^clubs/robotics$', 'sntc.views.robotics', name='robotics'),
+    url(r'^clubs/sae$', 'sntc.views.sae', name='sae'),
+    url(r'^clubs/cef$', 'sntc.views.cef', name='cef'),
+    url(r'^teams/trident$', 'sntc.views.trident', name='trident'),
+    url(r'^teams/vocowa$', 'sntc.views.vocowa', name='vocowa'),
+    url(r'^teams/auv$', 'sntc.views.auv', name='auv'),
+    url(r'^app/$', 'sntc.views.app', name='app'),
 ]
