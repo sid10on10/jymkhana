cb321e82c52e10a577d952c111891a58aa7347b7
Siddhant Singh <sid10on10@gmail.com>
2018-01-09 13:38:30 +0530

    sntc integration JS

--- static/sntc/js/custom.js
+++ static/sntc/js/custom.js
@@ -17,7 +17,7 @@ $(function() {
     })
 
 $("header").bgswitcher({
-  images: ["/static/img/header.jpg", "/static/img/header2.jpg", "/static/img/header3.jpg", "/static/img/header4.jpg", "/static/img/header5.jpg", "/static/img/header6.jpg"],
+  images: ["/static/sntc/img/header.jpg", "/static/sntc/img/header2.jpg", "/static/sntc/img/header3.jpg", "/static/sntc/img/header4.jpg", "/static/sntc/img/header5.jpg", "/static/sntc/img/header6.jpg"],
   effect: "fade",
   interval: 4000,
   duration: 1000
